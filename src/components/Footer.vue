687a6a5f4bb1caac192c216f77480c361353c47b
francescogentile25 <francescogentile250893@gmail.com>
2023-05-23 01:29:47 +0200

    FEAT  change Footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -22,46 +22,97 @@
                     </p>
                 </div>
             </div>
-            <div class="row justify-content-center">
-                <div class="col col-md-2 text-center mb-2">
-                    <a href="">
-                        <img src="https://picsum.photos/200/300" class="img-fluid">
-                        <p class="text-uppercase">lorem ipsum</p>
-                    </a>
-                </div>
-                <div class="col-12 col-md-2 text-center mb-2">
-                    <a href="">
-                        <img src="https://picsum.photos/200/300" class="img-fluid">
-                        <p class="text-uppercase">lorem ipsum</p>
-                    </a>
-                </div>
-                <div class="col-12 col-md-2 text-center mb-2">
-                    <a href="">
-                        <img src="https://picsum.photos/200/300" class="img-fluid">
-                        <p class="text-uppercase">lorem ipsum</p>
-                    </a>
-                </div>
-                <div class="col-12 col-md-2 text-center mb-2">
-                    <a href="">
-                        <img src="https://picsum.photos/200/300" class="img-fluid">
-                        <p class="text-uppercase">lorem ipsum</p>
-                    </a>
-                </div>
-                <div class="col-12 col-md-2 text-center mb-2">
-                    <a href="">
-                        <img src="https://picsum.photos/200/300" class="img-fluid">
-                        <p class="text-uppercase">lorem ipsum</p>
-                    </a>
+            <div class="container">
+                <div class="row justify-content-center gap-3">
+                    <!-- <div class="col col-md-2 text-center mb-2">
+                        <a href="">
+                            <img src="https://picsum.photos/200/300" class="img-fluid">
+                            <p class="text-uppercase">lorem ipsum</p>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-2 text-center mb-2">
+                        <a href="">
+                            <img src="https://picsum.photos/200/300" class="img-fluid">
+                            <p class="text-uppercase">lorem ipsum</p>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-2 text-center mb-2">
+                        <a href="">
+                            <img src="https://picsum.photos/200/300" class="img-fluid">
+                            <p class="text-uppercase">lorem ipsum</p>
+                        </a>
+                    </div>
+                    <div class="col-12 col-md-2 text-center mb-2">
+                        <a href="">
+                            <img src="https://picsum.photos/200/300" class="img-fluid">
+                            <p class="text-uppercase">lorem ipsum</p>
+                        </a>
+                    </div> -->
+                    <div class="card shadow-sm col-12 col-md-2 text-center mb-2" v-for="item in  items " :key="item.id">
+                        <div class="img-hover-zoom img-hover-zoom--colorize text-center">
+                            <img class="shadow rounded-circle" :src="item.imageSrc" alt="" style="aspect-ratio: 1;">
+                        </div>
+                        <div class="card-body">
+                            <div class="my-2 text-center">
+                                <p class="fs-5">{{ item.name }}</p>
+                            </div>
+                            <div class="box">
+                                <div>
+                                    <ul class="list-inline">
+                                        <li>
+                                            <a :href="item.githubLink">
+                                                <font-awesome-icon icon="fa-brands fa-github" bounce size="xl" />
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
         </div>
     </footer>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            items: [
+                {
+                    id: 1,
+                    name: "Antonio Cafeo",
+                    imageSrc: "/src/assets/Antonio-Cafeo-profile.jpg",
+                    githubLink: "https://github.com/francescogentile"
+                },
+                {
+                    id: 2,
+                    name: "BoolBerto",
+                    imageSrc: "/src/assets/BoolBerto-profile.jpg",
+                    githubLink: "https://github.com/francescogentile"
+                },
+                {
+                    id: 3,
+                    name: "Giorgio Bocchio",
+                    imageSrc: "/src/assets/Giorgio-Bocchio-profile.jpg",
+                    githubLink: "https://github.com/francescogentile"
+                },
+                {
+                    id: 4,
+                    name: "Alessandro Baldi Pardi",
+                    imageSrc: "/src/assets/Alessandro-Baldi-Pardi.jpg",
+                    githubLink: "https://github.com/francescogentile"
+                },
+                {
+                    id: 5,
+                    name: "Francesco Gentile",
+                    imageSrc: "/src/assets/profile-pic (1).png",
+                    githubLink: "https://github.com/francescogentile"
+                },
+            ]
+        };
+    }
 }
 </script>
 
@@ -87,5 +138,30 @@ a {
     .text {
         column-count: 2;
     }
+}
+
+
+.card {
+    border-radius: 1rem;
+}
+
+.img-hover-zoom--colorize img {
+    width: 150px;
+    margin-top: 20px;
+    padding: 1px;
+    transition: transform .5s;
+    filter: grayscale(100%);
+}
+
+.img-hover-zoom--colorize:hover img {
+    filter: grayscale(0);
+    transform: scale(1.05);
+}
+
+
+.box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
