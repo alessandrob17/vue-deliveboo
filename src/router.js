763556663c01eb6279de21727f99198747447b27
albertopiffer <albertopiffer96@gmail.com>
2023-05-26 16:32:53 +0200

    FIX router

--- src/router.js
+++ src/router.js
@@ -26,7 +26,7 @@ const router = createRouter({
             name: 'research',
             component: Research,
         },
-
+        {
             path: '/restaurant/:id',
             name: 'restaurants.show',
             component: RestaurantsShow,
