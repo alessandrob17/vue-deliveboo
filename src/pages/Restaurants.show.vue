0535c28a5b91f43249f79c56245ef565b968bbf1
francescogentile25 <francescogentile250893@gmail.com>
2023-05-29 11:59:53 +0200

    STYLE add container row e col to restaurants show

--- src/pages/Restaurants.show.vue
+++ src/pages/Restaurants.show.vue
@@ -1,43 +1,49 @@
 <template>
     <Default>
-        <div class="d-flex container">
-            <div class="row g-4 ps-5 pe-5">
-                <div class="col-9">
-                    <div v-for="dish in dishesList" :key="dish.id">
-                        <span class="card m-0 p-0" v-if="dish.restaurant_id == getIdRestaurant('id')">
-                            <img src="https://www.negroni.com/sites/negroni.com/files/styles/scale__1440_x_1440_/public/664x487_istock-504122954.jpg?itok=GolOLXC4"
-                                class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ dish.name }}</h5>
-                                <p class="card-text"> {{ dish.price }}</p>
-                                <div class="d-grid gap-2 col-6 mx-auto">
-                                    <button @click="addDishToCart(dish)">
-                                        <font-awesome-icon icon="fa-solid fa-cart-plus" />Aggiungi al carrello
-                                    </button>
+        <div class="container">
+            <div class="row">
+                <div class="col-7">
+                    <div class="row">
+                        <div v-for="dish in dishesList" :key="dish.id">
+                            <div class="card col" v-if="dish.restaurant_id == getIdRestaurant('id')">
+                                <img src="https://www.negroni.com/sites/negroni.com/files/styles/scale__1440_x_1440_/public/664x487_istock-504122954.jpg?itok=GolOLXC4"
+                                    class="card-img-top" alt="...">
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title">{{ dish.name }}</h5>
+                                    <p class="card-text m-0"> {{ dish.price }}</p>
+                                    <p class="card-text"> {{ dish.description }}</p>
+                                    <div class="">
+                                        <button @click="addDishToCart(dish)">
+                                            <font-awesome-icon icon="fa-solid fa-cart-plus" />Aggiungi al carrello
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-3">
-                <div class="card text-dark bg-light mb-3 me-5" style="flex-grow: 1; max-height:600px">
-                    <h3 class="card-header text-center">CARRELLO</h3>
-                    <div class="card-body">
-                        <button @click="removeAllDish()">
-                            <font-awesome-icon icon="fa-solid fa-trash-can" />
-                            Rimuovi tutti
-                        </button>
-                        <ul>
-                            <li v-for="(dish, index) in dishes" :key="dish.id">
-                                {{ dish.name }} {{ dish.price }} ({{ dish.quantity }})
-                                <button @click="removeDishFromCart(index)">
-                                    <font-awesome-icon icon="fa-solid fa-trash-can" />
-                                    Rimuovi
-                                </button>
-                            </li>
-                            <p>Somma totale: {{ totalPrice }}</p>
-                        </ul>
+                <div class="col-5">
+                    <div class="card text-dark bg-light">
+                        <h3 class="card-header text-center">CARRELLO</h3>
+                        <div class="card-body">
+                            <button @click="removeAllDish()">
+                                <font-awesome-icon icon="fa-solid fa-trash-can" />
+                                Rimuovi tutti
+                            </button>
+                            <ul>
+                                <li v-for="(dish, index) in dishes" :key="dish.id">
+                                    {{ dish.name }} {{ dish.price }} ({{ dish.quantity }})
+                                    <button @click="addDishToCart(dish)">
+                                        <font-awesome-icon icon="fa-solid fa-cart-plus" />Aggiungi
+                                    </button>
+                                    <button @click="removeDishFromCart(index)">
+                                        <font-awesome-icon icon="fa-solid fa-trash-can" />
+                                        Rimuovi
+                                    </button>
+                                </li>
+                                <p>Somma totale: {{ totalPrice }}</p>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
