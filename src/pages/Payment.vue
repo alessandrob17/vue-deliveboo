87ffd05ac656b0bbe0eb79b0cf22b3652542dc44
albertopiffer <albertopiffer96@gmail.com>
2023-05-31 16:07:27 +0200

    FEAT post funzionante inc

--- src/pages/Payment.vue
+++ src/pages/Payment.vue
@@ -7,29 +7,32 @@
                     <div class="alert alert-success" v-if="nonce">
                         Successfully generated nonce.
                     </div>
-                    <!-- <form @submit="postOrder">
+                    <form @submit="postOrder">
                         <div class="form-group">
                             <label>Nome</label>
-                            <input id="" class="form-control" placeholder="Inserisci Nome">
+                            <input type="text" v-model="name" class="form-control" placeholder="Inserisci Nome">
                         </div>
                         <div class="form-group">
                             <label>Indirizzo di consegna</label>
-                            <input id="" class="form-control" placeholder="Inserisci l'indirizzo di consegna">
+                            <input type="text" v-model="address" class="form-control"
+                                placeholder="Inserisci l'indirizzo di consegna">
                         </div>
                         <div class="form-group">
                             <div class="row">
                                 <div class="col-6">
                                     <label>Numero di telefono</label>
-                                    <input id="" class="form-control" placeholder="numero di telefono">
+                                    <input type="text" v-model="phone_number" class="form-control"
+                                        placeholder="numero di telefono">
                                 </div>
                                 <div class="col-6">
                                     <label>Totale</label>
-                                    <input id="" class="form-control" placeholder="Questo dato non deve essere un input">
+                                    <input type="text" v-model="total" class="form-control"
+                                        placeholder="Questo dato non deve essere un input">
                                 </div>
                             </div>
                         </div>
 
-                        <hr />
+                        <!-- <hr />
                         <div class="form-group">
                             <label>Numero carta di credito</label>
                             <div id="creditCardNumber" class="form-control"></div>
@@ -45,17 +48,20 @@
                                     <div id="cvv" class="form-control"></div>
                                 </div>
                             </div>
-                        </div>
-                        <button class="btn btn-primary btn-block">
+                        </div> -->
+
+
+                        <button class="btn btn-primary btn-block" type="submit">
                             Paga stronzo
                         </button>
-                    </form> -->
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import axios from 'axios';
 import braintree from 'braintree-web';
 
 export default {
@@ -63,50 +69,74 @@ export default {
         return {
             hostedFieldInstance: false,
             nonce: "",
+
+            name: '',
+            address: '',
+            phone_number: '',
+
+            total: '',
         }
     },
-    mounted() {
-        braintree.client.create({
-            authorization: "sandbox_q76fr39z_pwygckh8zgzr2g5y"
-        })
-            .then(clientInstance => {
-                let options = {
-                    client: clientInstance,
-                    styles: {
-                        input: {
-                            'font-size': '14px',
-                            'font-family': 'Open Sans'
-                        }
-                    },
-                    fields: {
-                        number: {
-                            selector: '#creditCardNumber',
-                            placeholder: 'Enter Credit Card'
+
+    methods: {
+        postOrder(event) {
+            event.preventDefault();
+
+            const formData = {
+                name: this.name,
+                address: this.address,
+                phone_number: this.phone_number,
+
+                total: this.total,
+            };
+
+            axios.post('http://localhost:8000/api/order-post', formData)
+                .then(response => {
+                    console.log(response.data);
+                    // Esegui altre azioni dopo il salvataggio dei dati
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        },
+
+        mounted() {
+            braintree.client.create({
+                authorization: "sandbox_q76fr39z_pwygckh8zgzr2g5y"
+            })
+                .then(clientInstance => {
+                    let options = {
+                        client: clientInstance,
+                        styles: {
+                            input: {
+                                'font-size': '14px',
+                                'font-family': 'Open Sans'
+                            }
                         },
-                        cvv: {
-                            selector: '#cvv',
-                            placeholder: 'Enter CVV'
-                        },
-                        expirationDate: {
-                            selector: '#expireDate',
-                            placeholder: '00 / 0000'
+                        fields: {
+                            number: {
+                                selector: '#creditCardNumber',
+                                placeholder: 'Enter Credit Card'
+                            },
+                            cvv: {
+                                selector: '#cvv',
+                                placeholder: 'Enter CVV'
+                            },
+                            expirationDate: {
+                                selector: '#expireDate',
+                                placeholder: '00 / 0000'
+                            }
                         }
                     }
-                }
-                return braintree.hostedFields.create(options)
-            })
-            .then(hostedFieldInstance => {
-                this.hostedFieldInstance = hostedFieldInstance;
-            })
-            .catch(err => {
-            });
-    },
-    methods: {
-        //Se vuoi lanciare entrambe le funzioni con un unico button
-        // sendOrder() {
-        //     this.payWithCreditCard();
-        //     this.postOrder();
-        // },
+                    return braintree.hostedFields.create(options)
+                })
+                .then(hostedFieldInstance => {
+                    this.hostedFieldInstance = hostedFieldInstance;
+                })
+                .catch(err => {
+                });
+        },
+
         payWithCreditCard() {
             if (this.hostedFieldInstance) {
                 this.hostedFieldInstance.tokenize().then(payload => {
@@ -126,5 +156,4 @@ export default {
 <style lang="scss" scoped>
 body {
     padding: 20px;
-}
-</style>
+}</style>
