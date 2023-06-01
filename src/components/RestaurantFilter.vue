f2988337a79a0d0fa030a4742ad68ae8b309a30d
albertopiffer <albertopiffer96@gmail.com>
2023-06-01 03:11:08 +0200

    FIX restaurantfilter

--- src/components/RestaurantFilter.vue
+++ src/components/RestaurantFilter.vue
@@ -1,28 +1,20 @@
 <template>
-  <div class="mb-5 filter-bg">
+  <div class="mb-5">
 
-    <!-- advanced search -->
-    <!-- 
-    <a class="d-flex justify-content-center" href="/research">
-      <button class="btn-search">Esegui una ricerca avanzata!</button>
-    </a> -->
     <AdvanceSearch></AdvanceSearch>
 
     <div class="box row">
-
       <div v-for="typology in typologies" :key="typology.id" class="col-3 p-2 m-auto">
         <!-- <a class="filter-link fs-2" :href='/research'>{{ typology.name }}</a> -->
         <a class="filter-link" @click="selectTypology(typology.id)">{{ typology.name }}</a>
-
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import axios from 'axios';
 import AdvanceSearch from './AdvanceSearch.vue';
+import axios from 'axios';
 
 import { router } from '../router';
 
@@ -53,36 +45,11 @@ export default {
   created() {
     this.fetchTypologies();
   },
-    data() {
-        return {
-            restaurants: [],
-            typologies: [],
-        };
-    },
-    methods: {
-        fetchRestaurants() {
-            axios
-                .get('http://127.0.0.1:8000/api/restaurants')
-                .then((res) => {
-                    this.restaurants = res.data.results;
-                });
-        },
-        fetchTypologies() {
-            axios
-                .get('http://127.0.0.1:8000/api/typologies')
-                .then((res) => {
-                    this.typologies = res.data.results;
-                });
-        }
-    },
-    created() {
-        this.fetchRestaurants();
-        this.fetchTypologies();
-    },
-})
+}
 </script>
 
 <style lang="scss" scoped>
+@use'../style/partials/reset.scss';
 @use '../style/partials/color.scss' as *;
 
 .box {
@@ -90,21 +57,13 @@ export default {
   width: 80vw;
 }
 
-a {
-  text-decoration: none;
-}
-
-.btn-search:hover {
-  background-color: $principale;
-}
-
 .filter-link {
   font-size: 14px;
 
   width: 100%;
-  background-color: white;
-  border-radius: 5px;
-  color: $principale;
+  background-color: $principale;
+  border-radius: 25px;
+  color: white;
   display: block;
   padding: 10px 5px;
   text-align: center;
@@ -112,13 +71,9 @@ a {
   transition: background-color 0.5s;
   transition: color 0.5s;
 }
-.filter-link:hover{
-  background-color: $principale;
-  color: white;
-}
 
-.filter-bg{
-  background-color: rgb(110, 110, 110);
-  padding: 100px;
+.filter-link:hover {
+  background-color: white;
+  color: $principale;
 }
 </style>
