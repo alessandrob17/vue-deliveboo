3de3a7e2bb3de72ffa9bcee1c05a3e51e8b105d4
francescogentile25 <francescogentile250893@gmail.com>
2023-06-03 12:25:34 +0200

    STYLE responsive front

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -14,23 +14,21 @@
 
     <div class="container d-flex align-items-center justify-content-center">
 
+
         <div class="container-logo-hero">
             <img class="logo-hero " src="../assets/462a03f57b4441e6c8e7079d4f3583f7-removebg.png" alt="">
 
-            
+
 
         </div>
 
-        <div >
+        <div>
             <h1 class="h1-hero text-center">
                 Ordina stronzo
             </h1>
         </div>
-        
+
     </div>
-
-    
-
 </template>
 
 <script>
@@ -65,52 +63,62 @@ export default {
     min-width: 100%;
     flex-basis: 50%;
     flex-wrap: wrap;
+    background-image: url(../assets/wave.svg);
+    background-repeat: no-repeat;
+    background-position: bottom;
 }
 
 .logo-hero {
     width: 465px;
 }
 
-.h1-hero{
+.h1-hero {
     position: relative;
     color: white;
     font-size: 100px;
     max-width: 600px;
-    text-transform: uppercase;  
-    font-family: Roboto;  
-    
+    text-transform: uppercase;
+    font-family: Roboto;
+
 }
 
-.container-logo-hero{
+.container-logo-hero {
     position: absolute;
 }
 
 
 @media screen and (min-width: 2560px) {
-  .h1-hero{
-    font-size: 100px;
-    max-width: 600px;
-  }
+    .h1-hero {
+        font-size: 100px;
+        max-width: 600px;
+    }
 
-  .logo-hero {
-    width: 550px;
+    .logo-hero {
+        width: 550px;
+    }
+
+    .container {
+        min-height: 715px;
+    }
+
 }
 
-.container{
-    min-height: 715px;
-}
-
-}
 @media screen and (max-width: 767px) {
-    .h1-hero{
-    font-size: 90px;
-    max-width: 600px;
-  }
+    .h1-hero {
+        font-size: 90px;
+        max-width: 600px;
+    }
 
-  .logo-hero {
-    width: 420px;
-}
-  
+    .logo-hero {
+        width: 420px;
+    }
+
+    .container {
+        background-position-y: 50vh;
+
+
+    }
+
 }
 
 
